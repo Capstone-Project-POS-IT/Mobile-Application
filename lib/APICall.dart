3c3b48ec8a3b883ea0deed5436c6674b9a27afec
Anthony Baron <antonbaron10@gmail.com>
2020-09-21 20:14:30 -0400

    API Test has been put into separate folder where all calls will be

---
+++ lib/APICall.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:html';
+import 'dart:io';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class APICall {
+  //initialize the firebase if not initialized
+  static Future<bool> initializeFirebase() async {
+    if (Firebase.apps.isEmpty) {
+      await Firebase.initializeApp();
+    }
+    return true;
+  }
+
+  //test the API
+  static Future<void> testAPI() async {
+    await initializeFirebase();
+    final HttpsCallable callable = CloudFunctions.instance.getHttpsCallable(
+      functionName: 'testDemo',
+    );
+    dynamic resp =
+        await callable.call(); //one time call function with no params
+    if (resp.data['msg'] == "Hello from Firebase!") {
+      print("API SUCCEEEDED!!!!!!!!!!!!!!!!!");
+    } else {
+      print("API failed :(");
+    }
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,61 +4,17 @@ import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pos_it/APICall.dart';
 
 
 void main(){
   runApp(MyApp());
 
   //test function call
-  Firebase.initializeApp().whenComplete(() =>testAPI());
-  sleep(const Duration(seconds:2));
-  Firebase.initializeApp().whenComplete(() => testDatabase());
+  APICall.testAPI();
 
 
 }
-//*****************************************Test Start **********************************************************/
-Future<void> testAPI() async {
-
-  final HttpsCallable callable = CloudFunctions.instance.getHttpsCallable(
-    functionName: 'testDemo',
-  );
-  dynamic resp = await callable.call(); //one time call function with no params
-  //function call with parameters
-  // dynamic resp = await callable.call(
-  //   <String, dynamic>{
-  //     'response': 'ping',
-  //   },
-  // );
-  if(resp.data['msg']=="Hello from Firebase!"){
-    print("API SUCCEEEDED!!!!!!!!!!!!!!!!!");
-  }
-  else{
-    print("API failed :(");
-  }
-}
-
-Future<void> testDatabase() async {
-  await Firebase.initializeApp();
-  FirebaseDatabase.instance.reference().child('AppName')
-      .set({
-    'title': 'POS.IT',
-    'created_at': DateTime.now().millisecondsSinceEpoch
-  });
-
-  var firebaseRef = FirebaseDatabase().reference().child('AppName');
-
-  await firebaseRef.once().then((DataSnapshot snapshot) {
-    var data = snapshot.value;
-    if(data['title']=='POS.IT'&&data['created_at']>0){
-      print("DATABASE SUCEEDED!!!!!!!!!!!!!!!!!!!!!!!!!");
-      firebaseRef.remove();
-    }
-  });
-}
-
-
-
-/*************************************************Test End**********************************************************/
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
