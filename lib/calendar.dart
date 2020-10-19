e9b2de737f14e7efaf9f4fac9814db955d4b2f08
Nathan Kennedy <nathankennedy122@gmail.com>
2020-10-19 15:38:42 -0400

    Worked on Calendar + Home View

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 
@@ -16,6 +17,7 @@ class _MainCalendarView extends State<MainCalendarView> {
   Map<DateTime, List<dynamic>> _events;
   double _currentSliderValue = 1;
   TextEditingController _eventController;
+  AnimationController _animationController;
 
   @override
   void initState() {
@@ -31,7 +33,7 @@ class _MainCalendarView extends State<MainCalendarView> {
           title: new Text('Daily Mood'),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(5.0),
+          padding: const EdgeInsets.all(8.0),
           child: SingleChildScrollView(
             child: Column(
               mainAxisSize: MainAxisSize.max,
@@ -44,19 +46,38 @@ class _MainCalendarView extends State<MainCalendarView> {
                             color: Colors.red,
                           ),
                         ),
-                  ),*/
+                  ), */
                   events: _events,
                   calendarController: _controller,
                   initialCalendarFormat: CalendarFormat.week,
                   formatAnimation: FormatAnimation.slide,
                   startingDayOfWeek: StartingDayOfWeek.sunday,
                   availableGestures: AvailableGestures.all,
+                  /*
+                  builders: CalendarBuilders(
+                    selectedDayBuilder: (context, date, _) {
+                      return FadeTransition(
+                        opacity: Tween(begin: 0.0, end: 1.0).animate(_animationController),
+                        child: Container(
+                          margin: const EdgeInsets.all(4.0),
+                          padding: const EdgeInsets.only(top: 5.0, left: 6.0),
+                          color: Colors.deepOrange[300],
+                          width: 100,
+                          height: 100,
+                          child: Text(
+                            '${date.day}',
+                            style: TextStyle().copyWith(fontSize: 16.0),
+                          ),
+                        ),
+                      );
+                    },
+                  ), */
                 ),
                 Slider(
                   value: _currentSliderValue,
                   min: 0,
                   max: 10,
-                  divisions: 100,
+                  divisions: 10,
                   label: _currentSliderValue.round().toString(),
                   onChanged: (double value) {
                     setState(() {
@@ -105,6 +126,33 @@ class _MainCalendarView extends State<MainCalendarView> {
       )
     );
   }
+
+  /*
+  Widget _buildEventsMarker(DateTime date, List events) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: _controller.isSelected(date)
+            ? Colors.brown[500]
+            : _controller.isToday(date)
+            ? Colors.brown[300]
+            : Colors.blue[400],
+      ),
+      width: 16.0,
+      height: 16.0,
+      child: Center(
+        child: Text(
+          '${events.length}',
+          style: TextStyle().copyWith(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+      ),
+    );
+  }
+  */
 
   //simple function that takes a number as a string input (1 -10) and returns a color
   Color getColor(String mood) {
