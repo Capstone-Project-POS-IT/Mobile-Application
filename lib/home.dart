64800e09d277b30ce32a3fcb08830f6b9c79d0b9
csc470-fall2019 <yousafit@yahoo.com>
2020-11-23 21:05:33 -0500

    update home UI

--- lib/home.dart
+++ lib/home.dart
@@ -11,13 +11,11 @@ import 'ExternalCalls.dart';
 class WelcomeName extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text("Welcome "+UserInformation.getDisplayName(),
-    textAlign: TextAlign.center,
-    style: TextStyle(fontSize: 40,color: Colors.blue));
+    return Text("Welcome " + UserInformation.getDisplayName(),
+        textAlign: TextAlign.center,
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40, color: Color(0xffDCFCDD)));
   }
 }
-
-
 
 class HomeView extends StatefulWidget {
   @override
@@ -28,32 +26,54 @@ class _HomeView extends State<HomeView> {
   @override
   Widget build(context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen'),
-      ),
+        backgroundColor: Color(0xff131D47),
         body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(100.0),
-            child: Center(
-              child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget> [WelcomeName() ,FutureBuilder<String> (
-                    future: APICall.getInspirationalQuote(),
-                    builder: (context, AsyncSnapshot<String> snapshot) {
-                      if (snapshot.hasData) {
-                        return Text(snapshot.data,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        );
-                      } else {
-                        return CircularProgressIndicator();
-                      }
+            child: Column(
+              children: <Widget> [
+                Title(),
+                WelcomeName(),
+                FutureBuilder<String> (
+                  future: APICall.getInspirationalQuote(),
+                  builder: (context, AsyncSnapshot<String> snapshot) {
+                    if (snapshot.hasData) {
+                      return Text(snapshot.data,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 20, color: Color(0xffffffff)),
+                      );
+                    } else {
+                      return CircularProgressIndicator();
                     }
-                )],
-              )
-            ),
-          ),
+                  }
+              )],
+            )
         )
+    );
+  }
+}
+
+class Title extends StatefulWidget {
+  @override
+  _Title createState() => _Title();
+}
+
+class _Title extends State<Title> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(15, 15, 0, 20),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Image.asset("lib/assets/images/posit_logo.png", height:150),
+          Align(
+            alignment: Alignment.topRight,
+            /*child: Text("Welcome",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Color(0xffDCFCDD), fontWeight: FontWeight.bold, fontSize: 30)),*/
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:pos_it/Authentication/EmailAuthentication.dart';
 import 'package:pos_it/Authentication/SignUp.dart';
 import 'package:pos_it/ExternalCalls.dart';
 import 'package:pos_it/Profile.dart';
+import 'package:pos_it/Settings.dart';
 import 'package:pos_it/creators.dart';
 import 'package:pos_it/home.dart';
 import 'package:pos_it/news.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Login(),
+      home: HomeView(),
     );
   }
 }
